7e708a8aa53a472e82e361bc7abf417644d3337a
Feichun Yang <feichuny0513@gmail.com>
2023-08-07 21:18:12 -0400

    Update site.config.ts
    update basic information of page and author

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,21 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b37f5c6210b246b388e59bb491d2b8c0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Archive',
+  domain: 'https://www.notion.so/katsukionly/b37f5c6210b246b388e59bb491d2b8c0',
+  author: 'K',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // social usernames (optional),
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
